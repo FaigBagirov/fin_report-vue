bcbae9680b33229a41ad5f34bf17c75b340f39d1
FaigBagirov <bagirov3105@gmail.com>
2021-09-21 17:11:18 +0400

    front ready, have to develop backend for the dataset

--- components/Report-card.vue
+++ components/Report-card.vue
@@ -1,12 +1,51 @@
 <template>
-<div class="report_card card card-body">
-    <p class="count">{{countOfOperations}}</p>
-    <p class="name">{{operationType}}</p>
+  <div class="report_card card card-body">
+    <p class="count">
+      {{ countOfOperations }}
+    </p>
+    <p class="name">
+      {{ operationType }}
+    </p>
     <!-- <button [routerLink]="['/navigator']" [queryParams]="{tab: tabNo}" class="seeall_btn btn btn-primary">See all</button> -->
-</div>
 
+    <nuxt-link :to="{ path: 'navigator', query: { tab: tabNo }}" tag="button" class="seeall_btn btn btn-primary">
+      See all
+    </nuxt-link>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    operationType: {
+      type: String,
+      required: true
+    },
+    countOfOperations: {
+      type: Number,
+      required: true
+    },
+    tabNo: {
+      type: Number,
+      required: true
+    }
+  }
+}
 </script>
+
+<style >
+    .count{
+        font-weight: bold;
+        text-align: left;
+    }
+    .name{
+        text-align: left;
+    }
+    .seeall_btn{
+        margin-left: auto;
+        margin-right: 10px;
+        min-width: 100px;
+        max-width: 200px;
+        width: 10vw;
+    }
+</style>
--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,18 +1,43 @@
 <template>
-<div class="card">
-    <div class="card-header"><p class="trancacs" style="opacity: 0;">sorry for this method, but I liked this idea of mine so much</p>
+  <div class="card">
+    <div class="card-header">
+      <p class="trancacs" style="opacity: 0;">
+        sorry for this method, but I liked this idea of mine so much
+      </p>
     </div>
     <div class="card-body">
-        <h1 class="welcome">Welcome</h1>
-        <p class="welcome_desc">With supporting text below as a natural lead-in to additional content</p>
-        <button type="button" class=" btn btn-primary">See Transactions</button>
+      <h1 class="welcome">
+        Welcome
+      </h1>
+      <p class="welcome_desc">
+        With supporting text below as a natural lead-in to additional content
+      </p>
+      <button type="button" class=" btn btn-primary">
+        See Transactions
+      </button>
     </div>
     <div class="card-footer">
-    <p class="trancacs">You have {{countOfOTransactions}} transactions</p>
+      <p class="trancacs">
+        You have {{ countOfTransactions }} transactions
+      </p>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    countOfTransactions: {
+      type: Number,
+      required: true
+    }
+  }
+
+}
 </script>
+
+<style>
+  .card-body, .card-footer{
+      text-align: center;
+  }
+</style>
